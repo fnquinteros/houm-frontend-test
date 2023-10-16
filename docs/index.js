01deac5b44ea1cfb909ee7e75df76983b77d1c9b
Felipe Quinteros <fnquinteros@uc.cl>
2023-10-16 12:46:21 +0200

    try gh-pages

---
+++ docs/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "../src/Main";
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(<Main />, document.getElementById("root"));
